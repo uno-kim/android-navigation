042def129c7774a394e1c45df8bac42dd1f11137
Min Sung Kim <ms.uno.kim@gmail.com>
2019-04-28 23:12:26 +0900

    first commit

---
+++ app/src/main/java/com/example/navigation/LandingPage.java
@@ -0,0 +1,38 @@
+package com.example.navigation;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class LandingPage extends Fragment {
+
+    private Button mButton;
+
+    public LandingPage() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_landing_page, container, false);
+        mButton = v.findViewById(R.id.button_next);
+        mButton.setOnClickListener(
+                Navigation.createNavigateOnClickListener(R.id.action_landing_page_frag_to_room_frag,
+                        null));
+        return v;
+    }
+
+}
